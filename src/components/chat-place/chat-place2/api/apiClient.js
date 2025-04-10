6027c8df5c327b48cfecfa6f343213e41a713d28
jorge loreto <jorge.loreto.2025@gmail.com>
2025-04-10 13:22:04 -0600

    removing important data

--- src/components/chat-place/chat-place2/api/apiClient.js
+++ src/components/chat-place/chat-place2/api/apiClient.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-//const API_BASE_URL = "http://localhost:8080";
-//const API_BASE_URL = "http://192.168.1.121:5000";
-//const API_BASE_URL = "https://34.51.36.4:5000";
-//const API_BASE_URL ="https://my-iteci-app-1095159323845.us-central1.run.app"
 const API_BASE_URL =process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 const apiClient = axios.create({
